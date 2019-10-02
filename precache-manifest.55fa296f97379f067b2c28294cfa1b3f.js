3b84e1d3787f7e6d2c2ab2bc43a093eb9ae10d16
Vadim <vadimchernev16@gmail.com>
2019-10-02 12:22:43 +0300

    prod build

---
+++ precache-manifest.55fa296f97379f067b2c28294cfa1b3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20193503a2e9466fc9e869d25bfd75d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7908b68c378f9e2107a2",
+    "url": "/static/css/main.12b7ef19.chunk.css"
+  },
+  {
+    "revision": "ca5ae286f2040e56f312",
+    "url": "/static/js/2.bb351494.chunk.js"
+  },
+  {
+    "revision": "7908b68c378f9e2107a2",
+    "url": "/static/js/main.26a78831.chunk.js"
+  },
+  {
+    "revision": "5abeae2679ca95e42ba5",
+    "url": "/static/js/runtime-main.3a74da99.js"
+  }
+]);
